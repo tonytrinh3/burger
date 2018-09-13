494e57976bd04d1d3a9e1c9ffbd2e987b3f04f90
Tony Trinh <tony.w.trinh3@gmail.com>
2018-09-13 08:07:34 -0700

    build control 9-13
    build control 9-13

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,7 +6,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 //object.keys turn object into array
 
 const burger = (props) =>{
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map(  (_,i) =>{
@@ -14,8 +14,16 @@ const burger = (props) =>{
 
                 }) 
 
-    });// [one element, two element]
+    })
+    .reduce((arr,el)=>{
+        return arr.concat(el)
+    }, []);// [one element, two element]
 
+    console.log(transformedIngredients);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+   
     return(
         <div className = {classes.Burger}>
             <BurgerIngredient type="bread-top"/>
