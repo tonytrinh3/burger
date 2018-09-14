eaed6a16f5dc5c0e7415af4170962c2b7cc5b732
Tony Trinh <tony.w.trinh3@gmail.com>
2018-09-14 08:13:24 -0700

    burger controls 9-14
    burger controls 9-14

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import classes from './BuildControls.css'
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
+
+
+
 
 //function component
 const buildControls = (props) => (
     <div className = {classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl 
+            key = {ctrl.label} 
+            label = {ctrl.label}
+            added = {() => props.ingredientAdded(ctrl.type)}
+            removed = {() => props.ingredientRemoved(ctrl.type)}
+            disabled = {props.disabled[ctrl.type]}
+            />
+
+
+        ))}
     </div>
 
 );
